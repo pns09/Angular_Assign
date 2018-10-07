793788390d23c37b66f6b361b047865523cdbee3
pns09 <s530715@nwmissouri.edu>
2018-10-07 16:56:06 -0500

    Angular_Assign

---
+++ src/app/SUN/sun.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+selector: '<sun-root>',
+template: `<p>sun is working fine</p>`,
+styles:[`
+p{
+    padding: 20px;
+    background-color:red;
+    border: 2px solid black;
+}`]
+})
+
+export class SunComponent{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import{FormsModule}from '@angular/forms';
 import { AppComponent } from './app.component';
+import { SunComponent } from './SUN/sun.component';
+import { MoonComponent } from './moon/moon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SunComponent,
+    MoonComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
